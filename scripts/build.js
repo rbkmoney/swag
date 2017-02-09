3f111e317fe99c799cf9e7e6ae1fea5a9a8faa72
Andrey Mayorov <a.mayorov@rbkmoney.com>
2017-02-09 23:26:11 +0300

    Add wercker ci jobs

--- scripts/build.js
+++ scripts/build.js
@@ -2,20 +2,23 @@
 'use strict';
 var Path = require('path');
 
+var TARGET_DIR = 'web_deploy'
+if (process.argv[2]) {
+    TARGET_DIR = process.argv[2]
+}
+
 require('shelljs/global');
 set('-e');
 
-mkdir('-p', 'web_deploy/wsd')
+mkdir('-p', TARGET_DIR + '/wsd')
 
-cp('-R', 'web/*', 'web_deploy/');
+cp('-R', 'web/*', TARGET_DIR + '/');
+cp('-R', 'spec/wsd/*', TARGET_DIR + '/wsd/');
 
-cp('-R', 'spec/wsd/*', 'web_deploy/wsd/');
-
-exec('npm run swagger bundle --        -o web_deploy/swagger.json');
-exec('npm run swagger bundle -- --yaml -o web_deploy/swagger.yaml');
+exec('npm run swagger bundle --        -o ' + TARGET_DIR + '/swagger.json');
+exec('npm run swagger bundle -- --yaml -o ' + TARGET_DIR + '/swagger.yaml');
 
 var SWAGGER_UI_DIST = Path.dirname(require.resolve('swagger-ui'));
-rm('-rf', 'web_deploy/swagger-ui/')
-cp('-R', SWAGGER_UI_DIST, 'web_deploy/swagger-ui/')
-sed('-i', 'http://petstore.swagger.io/v2/swagger.json', '../swagger.json', 'web_deploy/swagger-ui/index.html')
-
+rm('-rf', TARGET_DIR + '/swagger-ui/')
+cp('-R', SWAGGER_UI_DIST, TARGET_DIR + '/swagger-ui/')
+sed('-i', 'http://petstore.swagger.io/v2/swagger.json', '../swagger.json', TARGET_DIR + '/swagger-ui/index.html')
