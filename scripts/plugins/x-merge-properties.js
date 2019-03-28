3cf9b6623e9a0a15fb9f11251742055676a4108c
k.struzhkin <k.struzhkin@rbkadmins-MacBook-Pro.local>
2019-03-28 19:12:08 +0300

    Fix after review

--- scripts/plugins/x-merge-properties.js
+++ scripts/plugins/x-merge-properties.js
@@ -7,27 +7,23 @@ module.exports = {
     console.log('* x-merge-properties plugin');
   },
   process: function(parent, name, jsonpath, swagger) {
-    console.log('jsonpath' + jsonpath);
-
     var value = parent[name];
     if (!Array.isArray(value)) {
       throw Error('x-merge-properties argument should be array at ' + jsonpath);
     }
-    let props = {};
     let required = [];
+    let properties = {};
     value.forEach(function(obj) {
-      if (typeof obj !== 'object') throw Error('Can\'t merge non-object values at ' + jsonpath);
       if (obj.$ref && (typeof obj.$ref === 'string')) {
         obj = jpointer.get(swagger, obj.$ref.substring(1));
       }
-      if (!obj.properties) throw Error('Can\'t merge properties is undefiend ' + jsonpath);
-      props = mergePatch.apply(props, obj.properties);
-      required = required.concat(obj.required);
+      if (typeof obj !== 'object') throw Error('Can\'t merge non-object values at ' + jsonpath);
+      required = required.concat(obj.required || []);
+      properties = mergePatch.apply(properties, obj.properties || {});
     });
     delete parent[name];
-    Object.assign(parent.properties, props);
-    let arr = parent.required.concat(required);
-    Object.assign(parent.required, arr);
+    parent.required = required;
+    parent.properties = properties;
   },
   finish: function(swagger) {
     // TODO: cleanup unused $refs
